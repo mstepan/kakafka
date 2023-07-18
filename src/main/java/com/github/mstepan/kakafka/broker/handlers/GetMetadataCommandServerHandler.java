a3707b2c6e9a1febe8d546e8beae88ea3fb751c1
mstepan <maksym.stepanenko@oracle.com>
2023-07-18 10:33:37 +0300

    GetMetadata code fixed

--- src/main/java/com/github/mstepan/kakafka/broker/handlers/GetMetadataCommandServerHandler.java
+++ src/main/java/com/github/mstepan/kakafka/broker/handlers/GetMetadataCommandServerHandler.java
@@ -7,12 +7,14 @@ import com.github.mstepan.kakafka.command.Command;
 import com.github.mstepan.kakafka.command.GetMetadataCommand;
 import com.github.mstepan.kakafka.command.response.MetadataCommandResponse;
 import io.etcd.jetcd.KeyValue;
+import io.etcd.jetcd.kv.GetResponse;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
 
 public class GetMetadataCommandServerHandler extends ChannelInboundHandlerAdapter {
 
@@ -31,56 +33,60 @@ public class GetMetadataCommandServerHandler extends ChannelInboundHandlerAdapte
         Command command = (Command) msg;
 
         if (command instanceof GetMetadataCommand) {
+            System.out.printf("[%s] 'get_metadata' command received %n", brokerName);
+
+            metadata.getMetadataState().whenComplete(new MetadataListener(ctx, msg));
+        } else {
+            ctx.fireChannelRead(msg);
+        }
+    }
+
+    private final class MetadataListener implements BiConsumer<GetResponse, Throwable> {
+
+        private final ChannelHandlerContext ctx;
+        private final Object msg;
+
+        public MetadataListener(ChannelHandlerContext ctx, Object msg) {
+            this.ctx = ctx;
+            this.msg = msg;
+        }
+
+        @Override
+        public void accept(GetResponse getResp, Throwable ex) {
             try {
-                System.out.printf("[%s] 'get_metadata' command received %n", brokerName);
+                if (ex != null) {
+                    System.out.printf(
+                            "[%s] getting metadata state from 'etcd' FAILED %n", brokerName);
+                    ex.printStackTrace();
 
-                metadata.getMetadataState()
-                        .whenComplete(
-                                (getResp, ex) -> {
-                                    if (ex != null) {
-                                        System.out.printf(
-                                                "[%s] getting metadata state from 'etcd' FAILED %n",
-                                                brokerName);
-                                        ex.printStackTrace();
+                    // TODO: write failed response here
+                    // ctx.writeAndFlush(new MetadataCommandResponse(null));
 
-                                        // TODO: write failed response here
-                                        // ctx.writeAndFlush(new MetadataCommandResponse(null));
+                    return;
+                }
 
-                                        return;
-                                    }
+                System.out.printf("[%s] metadata state obtained from 'etcd' %n", brokerName);
 
-                                    System.out.printf(
-                                            "[%s] metadata state obtained from 'etcd' %n",
-                                            brokerName);
+                List<LiveBroker> liveBrokers = new ArrayList<>();
+                for (KeyValue keyValue : getResp.getKvs()) {
 
-                                    List<LiveBroker> liveBrokers = new ArrayList<>();
-                                    for (KeyValue keyValue : getResp.getKvs()) {
+                    String brokerIdPath = keyValue.getKey().toString(StandardCharsets.US_ASCII);
 
-                                        String brokerIdPath =
-                                                keyValue.getKey()
-                                                        .toString(StandardCharsets.US_ASCII);
+                    final String brokerName =
+                            brokerIdPath.substring(brokerIdPath.lastIndexOf("/") + 1);
+                    final String brokerUrl =
+                            keyValue.getValue().toString(StandardCharsets.US_ASCII);
 
-                                        final String brokerName =
-                                                brokerIdPath.substring(
-                                                        brokerIdPath.lastIndexOf("/") + 1);
-                                        final String brokerUrl =
-                                                keyValue.getValue()
-                                                        .toString(StandardCharsets.US_ASCII);
+                    liveBrokers.add(new LiveBroker(brokerName, brokerUrl));
+                }
 
-                                        liveBrokers.add(new LiveBroker(brokerName, brokerUrl));
-                                    }
+                MetadataState state =
+                        new MetadataState(metadata.getLeaderBrokerName(), liveBrokers);
 
-                                    MetadataState state =
-                                            new MetadataState(
-                                                    metadata.getLeaderBrokerName(), liveBrokers);
-
-                                    ctx.writeAndFlush(new MetadataCommandResponse(state));
-                                });
+                ctx.writeAndFlush(new MetadataCommandResponse(state));
             } finally {
                 ReferenceCountUtil.release(msg);
             }
-        } else {
-            ctx.fireChannelRead(msg);
         }
     }
 
